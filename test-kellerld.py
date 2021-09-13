ed9d42af67a88614a440c0bd918ee1924dc1e164
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-13 15:50:44 +1000

    test-kellerld: use context manager, pathlib

--- test-kellerld.py
+++ test-kellerld.py
@@ -2,41 +2,31 @@
 
 import argparse
 from kellerLD import KellerLD
-from llog import *
-import os
-import signal
-import subprocess
+from pathlib import Path
+import llog
 import time
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-defaultMeta = dir_path+'/kellerld.meta'
+device = "kellerld"
+defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--bus', action='store', type=int, required=True)
-parser.add_argument('--frequency', action='store', type=int,
-                    default=5, help="set the measurement frequency")
+parser = argparse.ArgumentParser(description=f'{device} test')
 parser.add_argument('--output', action='store', type=str, default=None)
 parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
+parser.add_argument('--frequency', action='store', type=int, default=5,
+                    help="set the measurement frequency")
 args = parser.parse_args()
 
-log = LLogWriter(args.meta, args.output)
 
-def cleanup(_signo, _stack):
-    log.close()
-    exit(0)
+with llog.LLogWriter(args.meta, args.output) as log:
+    keller = KellerLD(args.bus)
+    keller.init()
+    log.log(llog.LLOG_ROM, f'{keller.month} {keller.day} {keller.year} {keller.pMin} {keller.pMax} {keller.pModeID}')
 
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-keller = KellerLD(args.bus)
-keller.init()
-log.log(LLOG_ROM, f'{keller.month} {keller.day} {keller.year} {keller.pMin} {keller.pMax} {keller.pModeID}')
-
-while True:
-    try:
-        keller.read()
-        log.log(LLOG_DATA, f'{keller.pressure():.6f} {keller.temperature():.2f}')
-    except Exception as e:
-        log.log(LLOG_ERROR, e)
-    if args.frequency:
-        time.sleep(1.0/args.frequency)
+    while True:
+        try:
+            keller.read()
+            log.log(llog.LLOG_DATA, f'{keller.pressure():.6f} {keller.temperature():.2f}')
+        except Exception as e:
+            log.log(llog.LLOG_ERROR, e)
+        if args.frequency:
+            time.sleep(1.0/args.frequency)
