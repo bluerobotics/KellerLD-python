8a901a10759950bb13f97cb8039e228c1fbb8b39
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 15:29:07 +1000

    test-kellerld: simplify with abstracted logging

--- test-kellerld.py
+++ test-kellerld.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
 
-import argparse
 from kellerLD import KellerLD
-from pathlib import Path
-import llog
-import time
+from llog import LLogWriter
 
 device = "kellerld"
-defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
-
-parser = argparse.ArgumentParser(description=f'{device} test')
-parser.add_argument('--output', action='store', type=str, default=None)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--frequency', action='store', type=int, default=5,
-                    help="set the measurement frequency")
+parser = LLogWriter.create_default_parser(__file__, device, default_frequency=5)
 args = parser.parse_args()
 
 
 with llog.LLogWriter(args.meta, args.output) as log:
     keller = KellerLD(args.bus)
     keller.init()
-    log.log(llog.LLOG_ROM, f'{keller.month} {keller.day} {keller.year} {keller.pMin} {keller.pMax} {keller.pModeID}')
-
-    while True:
-        try:
-            keller.read()
-            log.log(llog.LLOG_DATA, f'{keller.pressure():.6f} {keller.temperature():.2f}')
-        except Exception as e:
-            log.log(llog.LLOG_ERROR, e)
-        if args.frequency:
-            time.sleep(1.0/args.frequency)
+    log.log_rom(f'{keller.month} {keller.day} {keller.year} {keller.pMin} {keller.pMax} {keller.pModeID}')
+    
+    def data_getter():
+      keller.read()
+      return f'{keller.pressure():.6f} {keller.temperature():.2f}'
+    
+    log.log_data_loop(data_getter, parser_args=args)
