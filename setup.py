89845cf61abff328b33903cf457f8c15273c0ce6
Tom McCabe <tom.mccabe@missionsystems.com.au>
2022-12-07 15:07:09 +1100

    setup.py: Updated with install_requires, description, url, license and (optionally) maintainer info

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-from distutils.core import setup
-setup(name='kellerLD',
-      version='1.0',
-      py_modules=['kellerLD'],
-      )
+import setuptools
+
+setuptools.setup(
+      name              ='kellerLD',
+      version           ='1.0',
+      py_modules        =['kellerLD'],
+      install_requires  =['smbus'],
+      description       = 'A python driver for the Keller LD line of pressure sensors.',
+      url               = 'https://github.com/bluerobotics/KellerLD-python',
+      license           = 'TODO'
+      #maintainer          = '',
+      #maintainer_email    = '',
+)
